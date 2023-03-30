cb241cc5709cb7149dca89df94c19fc0b5f49258
Giannidluca93 <gdluca93@gmail.com>
2023-03-30 16:10:29 -0300

    Import basic Mainlayout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,14 @@
+import { MainLayout } from "@/components/layouts";
+
 import Typography from "@mui/material/Typography";
 
 export default function Home() {
-  return <Typography variant="h1">HelloWorld</Typography>;
+  return (
+    <MainLayout
+      title={"Photo Gallery - Home"}
+      pageDescription={"Photos categories"}
+    >
+      <Typography variant="h1">HelloWorld</Typography>;
+    </MainLayout>
+  );
 }
