d4b9e6b927df7c64dc8b62f8bbb42407e64489f0
Giannidluca93 <gdluca93@gmail.com>
2023-04-04 20:11:37 -0300

    setup link

--- components/Category.tsx
+++ components/Category.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
 
 import Image from "next/image";
+import NextLink from "next/link";
 
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
 
 import { ICategory } from "interfaces";
 
@@ -16,13 +18,23 @@ const Category: FC<Props> = ({ category }) => {
   return (
     <Grid item xs={12} sm={4}>
       <Box display="flex" flexDirection="column" alignItems="center">
-        <Image
-          src={category.profileImg}
-          alt={category.title}
-          width={150}
-          height={150}
-        />
-        <Typography>{category.title}</Typography>
+        <NextLink
+          href={`/categories/${category.title}`}
+          passHref
+          legacyBehavior
+        >
+          <Link color="text.secondary">
+            <Image
+              src={category.profileImg}
+              alt={category.title}
+              width={150}
+              height={150}
+            />
+            <Typography variant="subtitle1" align="center">
+              {category.title}
+            </Typography>
+          </Link>
+        </NextLink>
       </Box>
     </Grid>
   );
