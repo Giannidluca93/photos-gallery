1cb26811d6a67ebf7047e81a1a465bf2a4ccd972
Giannidluca93 <gdluca93@gmail.com>
2023-04-06 20:26:22 -0300

    use modal hook

--- components/CategoryImage.tsx
+++ components/CategoryImage.tsx
@@ -3,13 +3,21 @@ import Image from "next/image";
 
 import Box from "@mui/material/Box";
 
+import { useModal } from "hooks/useModal";
+
 interface Props {
   image: string;
 }
 
 const CategoryImage: FC<Props> = ({ image }) => {
+  const { open, toggleModal } = useModal();
   return (
-    <Box key={image} m={2} sx={{ cursor: "pointer" }}>
+    <Box
+      key={image}
+      m={2}
+      sx={{ cursor: "pointer" }}
+      onClick={() => toggleModal(open)}
+    >
       <Image src={image} height={150} width={150} alt="category image" />
     </Box>
   );
